d0a52d502fe5559baf5cf36be89b2b57ad7cc9c1
DANIEL ACOSTA <est.daniel.aacosta@unimilitar.edu.co>
2026-05-08 15:22:17 +0000

    Algoritmo Cohen-Sutherland implementado

--- script.js
+++ script.js
@@ -80,3 +80,115 @@ function ObtenerCodigo(x, y, xmin, ymin, xmax, ymax)
     }
     return codigo;
 }
+function CohenSutherland(x1, y1, x2, y2, xmin, ymin, xmax, ymax)
+{
+    let codigo1 = ObtenerCodigo(x1, y1, xmin, ymin, xmax, ymax);
+
+    let codigo2 = ObtenerCodigo(x2, y2, xmin, ymin, xmax, ymax);
+
+    let aceptada = false;
+
+    while(true)
+    {
+        // totalmente dentro
+        if((codigo1 | codigo2) === 0)
+        {
+            aceptada = true;
+            break;
+        }
+        // totalmente fuera
+        else if((codigo1 & codigo2) !== 0)
+        {
+            break;
+        }
+        // parcialmente visible
+        else
+        {
+            let codigoFuera;
+            let x;
+            let y;
+            // escoger punto fuera
+            if(codigo1 !== 0)
+            {
+                codigoFuera = codigo1;
+            }
+            else
+            {
+                codigoFuera = codigo2;
+            }
+            // arriba
+            if(codigoFuera & TOP)
+            {
+                x = x1 + (x2 - x1) * (ymax - y1) / (y2 - y1);
+
+                y = ymax;
+            }
+            // abajo
+            else if(codigoFuera & BOTTOM)
+            {
+                x = x1 + (x2 - x1) * (ymin - y1) / (y2 - y1);
+
+                y = ymin;
+            }
+            // derecha
+            else if(codigoFuera & RIGHT)
+            {
+                y = y1 + (y2 - y1) * (xmax - x1) / (x2 - x1);
+
+                x = xmax;
+            }
+            // izquierda
+            else if(codigoFuera & LEFT)
+            {
+                y = y1 + (y2 - y1) * (xmin - x1) / (x2 - x1);
+
+                x = xmin;
+            }
+            // actualizar primer punto
+            if(codigoFuera === codigo1)
+            {
+                x1 = Math.round(x);
+                y1 = Math.round(y);
+
+                codigo1 = ObtenerCodigo(
+                    x1,
+                    y1,
+                    xmin,
+                    ymin,
+                    xmax,
+                    ymax
+                );
+            }
+            // actualizar segundo punto
+            else
+            {
+                x2 = Math.round(x);
+                y2 = Math.round(y);
+
+                codigo2 = ObtenerCodigo(
+                    x2,
+                    y2,
+                    xmin,
+                    ymin,
+                    xmax,
+                    ymax
+                );
+            }
+        }
+    }
+    // línea aceptada
+    if(aceptada)
+    {
+        return {
+            visible:true,
+            x1,
+            y1,
+            x2,
+            y2
+        };
+    }
+    // línea rechazada
+    return {
+        visible:false
+    };
+}

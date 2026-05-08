97e8f8309f794baa43ec543e8fbcd8df390fa6b3
DANIEL ACOSTA <est.daniel.aacosta@unimilitar.edu.co>
2026-05-08 14:49:09 +0000

    Sistema de escenas Agregado

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-console.log("PROYECTO")
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-const WIDTH = canvas.width;
 const HEIGHT = canvas.height;
+const escenas =[]; // Almacena todas las lineas procesadas
 
+let EscenaActual = 0; //Indica cual mostrar
+const INDIDE = 0;
+const LEFT = 1;
+const RIGHT = 2;
+const BOTTOM =4;
+const TOP = 8;
 function transformarY(y)
 {
     return HEIGHT-y; //Para que el (0,0) empiece en la parte inferior derecha

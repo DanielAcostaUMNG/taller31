46d9c38b21c9e7f29d6a60cbe8bb07c0f3b8bbd0
DANIEL ACOSTA <est.daniel.aacosta@unimilitar.edu.co>
2026-05-08 15:05:05 +0000

    Estructura HTML y funciones base de dibujo corregidas

--- script.js
+++ script.js
@@ -1,47 +1,57 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-const HEIGHT = canvas.height;
-const escenas =[]; // Almacena todas las lineas procesadas
 
-let EscenaActual = 0; //Indica cual mostrar
-const INDIDE = 0;
+const WIDTH = canvas.width;
+const HEIGHT = canvas.height;
+
+const escenas = [];
+
+let escenaActual = 0;
+
+const INSIDE = 0;
 const LEFT = 1;
 const RIGHT = 2;
-const BOTTOM =4;
+const BOTTOM = 4;
 const TOP = 8;
 
-function DibujarViewport(xmin,ymin, xmax, ymax)
+function DibujarViewport(xmin, ymin, xmax, ymax)
 {
     ctx.strokeStyle = "blue";
-    ctx.strtokeRect(xmin, HEIGHT - ymax, xmax-xmin, ymax-ymin);
-    
+
+    ctx.strokeRect(
+        xmin,
+        HEIGHT - ymax,
+        xmax - xmin,
+        ymax - ymin
+    );
+
     // eje X
     ctx.beginPath();
-    ctx.moveTo(0,HEIGHT);
-    ctx.lineTo(canvas.WIDTH, HEIGHT);
+    ctx.moveTo(0, HEIGHT - 1);
+    ctx.lineTo(WIDTH, HEIGHT - 1);
     ctx.strokeStyle = "green";
     ctx.stroke();
-    
-    //eje Y
+
+    // eje Y
     ctx.beginPath();
-    ctx.moveTo(0,0);
+    ctx.moveTo(0, 0);
     ctx.lineTo(0, HEIGHT);
     ctx.stroke();
-
-
 }
-function DibujarLinea(x1,y1,x2,y2, color)
+
+function DibujarLinea(x1, y1, x2, y2, color)
 {
     ctx.strokeStyle = color;
+
     ctx.beginPath();
+
     ctx.moveTo(x1, HEIGHT - y1);
-    ctx.lineTo(x2, HEIGHT - 21);
-    ctx.stroke(); 
+    ctx.lineTo(x2, HEIGHT - y2);
+
+    ctx.stroke();
 }
 
 function LimpiarCanvas()
 {
-    ctx.clearRect(0,0,WIDTH,HEIGHT);
-    DibujarEjes();
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
 }
-DibujarEjes();
